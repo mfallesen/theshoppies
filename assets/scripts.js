ef19e1e32d2c7c58324d7f900e887dbde92b2f29
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-04 18:44:50 -0800

    added search functionality to the page, enabled debounce function to allow for typesearch, render search cards onto page and handle error

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,114 @@
 
 // be sure to replace the search term with the input field value
-const query_url = `http://www.omdbapi.com/?apikey=2afe3024&s=Matrix&type=movie`
+
 
 const searchBtn = document.getElementById('searchForMovie')
 
-async function findMovie(event) {
-    event.preventDefault()
-    console.log("Click");
-    fetch(query_url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch((err) => {
-        console.error("Error: ", err);
-    } )
+const debounce = (callback, delay) => {
+    let timeout = null
+    return (...args) => {
+        const next = () =>
+            callback(...args);
+        clearTimeout(timeout);
+        //1.   
+        timeout = setTimeout(next, delay)
+    }
 }
 
-searchBtn.onclick = findMovie;
+//   const debounce = (callback, time) => {
+//     let interval;
+//     return (...args) => {
+//       clearTimeout(interval);
+//       interval = setTimeout(() => {
+//         interval = null;
+//         callback(...args);
+//       }, time);
+//     };
+//   };
+
+async function findMovie(event) {
+    event.preventDefault()
+
+    const resultBox = document.getElementById('movie-results')
+    resultBox.innerHTML = "";
+
+    let movie = document.getElementById("searchBox").value
+    const query_url = `http://www.omdbapi.com/?apikey=2afe3024&s=${movie}&type=movie`
+
+
+    fetch(query_url)
+        .then(response => response.json())
+        .then((data) => {
+            // console.log(data);
+            let options = data.Search;
+            // console.log(options);
+
+            if (options != undefined) {
+
+                // loop through options and make cards for each
+                for (let i = 0; i < options.length; i++) {
+
+                    const title = options[i].Title;
+                    const year = options[i].Year;
+                    let poster = options[i].Poster;
+
+                    if (poster === 'N/A') {
+                        poster = "./assets/images/picture-not-available-clipart-12.jpg"
+                    }
+
+                    console.log(resultBox);
+
+                    const newCard = `<div class="flex-center card movie-card">
+                                        <h3 id="movieNo">${title}</h2>
+                                        <p id="movieYear">${year}</p>
+                                        <img src="${poster}" alt="${title} poster" id="moviePoster">
+                                        <button id="nominateMovie">Nominate me!</button>
+                                    </div>`
+
+                    resultBox.insertAdjacentHTML('beforeend', newCard);
+
+                }
+            } else {
+                const noMovie = `<div class="flex-center no-movie ">
+                                    <h2>Sorry we cant seem to find the movie you're looking for. Please enjoy this gif of nicholas Cage instead
+                                    </h2>
+                                    <img src="https://www.placecage.com/gif/300/300" alt="Nick Cage Placeholder">
+                                </div>`
+
+                resultBox.insertAdjacentHTML('beforeend', noMovie)
+            }
+
+        })
+        .catch((err) => {
+            console.error("Error: ", err);
+        })
+}
+
+let searchBox = document.getElementById('searchBox')
+
+
+const debouncedHandler = debounce(findMovie, 1000)
+
+
+
+
+
+searchBox.addEventListener('input', debouncedHandler)
+
+
+
+
+
+
+
+
+// function bounce(evt) {
+//     debounce(findMovie(evt), 1000);
+// }
+
+
+// , (evt) => {
+//     console.log("eventlistener fire");
+//     debounce(findMovie(evt), 1000);
+// }
+// searchBtn.onclick = findMovie;
